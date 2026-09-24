8b1b519b93db9605b9f8baeed5031a32791495ce
dishant1123 <dishantshah1417@gmail.com>
2026-09-24 11:24:05 +0530

    ....

---
+++ conditional  statement/24sept.py
@@ -0,0 +1,30 @@
+"""
+task :1 
+
+ask user to enter the  3 subjects marks  and calculate the  percentage  and based on percentage  give the  grade.
+
+Grade     percentage 
+above 90   A+ 
+80-90      A 
+70-80      B+
+60-70      B
+50-60      C+
+40-50      C
+below 40   Fail
+
+task :2 
+ask user to enter to enter the  salary and calculate the basic  salary. 
+
+basic salary  = salary  +HRA +DA 
+
+range           HRA     DA 
+<10000          20%     60% 
+10000-20000     25%     75%
+above 20000     30%     80%
+
+task :3 
+ask user to enter the character/string and check it's  vowel or  consonant or digits or special character. 
+
+
+
+"""
